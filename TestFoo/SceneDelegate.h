bd1b25155bc9234f3b216fe8122b77c34a6761e6
ppp <piase1910@163.com>
2022-02-09 22:21:42 +0800

    Initial Commit

---
+++ TestFoo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestFoo
+//
+//  Created by apple on 2022/2/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestFooUITests/TestFooUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  TestFooUITestsLaunchTests.m
+//  TestFooUITests
+//
+//  Created by apple on 2022/2/9.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestFooUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation TestFooUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
